51d9fcf4da2bc95d186f27f0878af3624f42c44f
wassimtajeddin <wassim.tajeddin@gmail.com>
2025-02-02 14:28:13 +0100

    Add index.html

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello world!"
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}"
 
 @app.route("/contact")
 def contact():
     return "Du kan kontakta mig på 0736842961"
 
-@app.reoute ("/about")
+@app.route ("/about")
 def about():
     return "En engagerad Java-utvecklare med en stark drivkraft att ständigt lära sig och utvecklas."
+
+@app.route("/index")
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
